631f0a4d746a67a86f3dc3848ee4e5133f86f03b
psb <939392270@qq.com>
2023-03-24 19:42:11 +0800

    update:更新demo 实现打开京东页面输获取到元素框并输入手机进行搜索

--- electron/utils.js
+++ electron/utils.js
@@ -10,12 +10,27 @@ exports = module.exports = class utils {
         console.log(title, '收到的值')
         //   A()
         win.setTitle(title)
-        const browser = await puppeteer.launch({ headless: false }); // default is true
+        const browser = await puppeteer.launch({
+            headless: false,
+            defaultViewport: null,
+            args: ['--start-maximized'],
+            ignoreDefaultArgs: ['--enable-automation']
+        }); // default is true
         const page = await browser.newPage();
-        page.setViewport({
-            width: 1920,
-            height:1080
-        })
-        await page.goto('https://xr-test.gacmotor.com/trumpchi');
+        // page.setViewport({
+        //     width: 1600,
+        //     height:800
+        // })
+        await page.goto("https://www.jd.com");
+        //获取输入框元素并在输入框内输入‘手机’
+        const input = await page.$('#key');
+        await input.type('手机');
+        //模拟键盘“回车”键
+        await page.keyboard.press('Enter');
+        //等待元素加载成功
+        await page.waitForSelector('#J_goodsList > ul > li:nth-child(1)');
+        //获取元素innerText属性
+        const firstText = await page.$eval('#J_goodsList > ul > li:nth-child(1)', el => el.innerText);
+        console.log('firstText', firstText);
     }
 }
